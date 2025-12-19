6dfa3cc08d218554d002f6cdb162c6da9dfd61e4
Abdelaziz Fitouri <medaziz.fitouri@gmail.com>
2025-12-19 16:25:33 +0100

    To see ASAP

---
+++ Traitement_SOP.py
@@ -0,0 +1,14 @@
+#import pandas as pd
+
+#sop = pd.read_excel("SOP Prévisionnel - 2026.xlsx")
+
+#pd.head()
+
+import sys
+
+print(sys.executable)
+print(sys.version)
+print(sys.path)
+
+#I HAVEN'T BEEN ABLE TO INSTALL ANY PACKAGES FROM THE WORK COMPUTER
+#I HAVE TO DO THIS FROM THE HOME COMPUTER
